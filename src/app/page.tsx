7e0baf7c44d15a7959265c104eb5936705b28a12
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-02-16 12:40:50 +0200

    ShadCN UI integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='text-2xl font-bold'>Hello world</h1>
+      <p>This is a simple NextJS Project with BetterAuth authentication.</p>
+    </div>
   )
 }
