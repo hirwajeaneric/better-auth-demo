f8ad58ecf788be2a335d4ddfa5109fd757a7e1a3
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-02-16 18:53:27 +0200

    Integration of GitHub Authentication

--- src/app/(auth)/auth/sign-up/page.tsx
+++ src/app/(auth)/auth/sign-up/page.tsx
@@ -9,8 +9,37 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { SignUpForm } from "@/components/auth/signup-form";
+import { signIn } from "@/lib/auth-client";
 
 export default function page() {
+  const SignInWithGithub = async () => {
+    await signIn.social({
+      /**
+       * The social provider id
+       * @example "github", "google", "apple"
+       */
+      provider: "github",
+      /**
+       * a url to redirect after the user authenticates with the provider
+       * @default "/"
+       */
+      callbackURL: "/dashboard",
+      /**
+       * a url to redirect if an error occurs during the sign in process
+       */
+      errorCallbackURL: "/auth",
+      /**
+       * a url to redirect if the user is newly registered
+       */
+      newUserCallbackURL: "/dashboard",
+      /**
+       * disable the automatic redirect to the provider. 
+       * @default false
+       */
+      disableRedirect: true,
+    });
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <Card>
@@ -23,12 +52,12 @@ export default function page() {
         <CardContent>
           <div>
             <div className="grid gap-6">
-              <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
+              <form onSubmit={SignInWithGithub} className="flex flex-col gap-4">
+                <Button type="submit" variant="outline" className="w-full">
                   <FaGithub />
                   Login with GitHub
                 </Button>
-              </div>
+              </form>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
                   Or continue with
